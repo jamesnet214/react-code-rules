7504efe8b00de21d060b168d25019b0fa7693c70
james lee <james.lee@devncore.org>
2021-09-23 05:14:11 +0900

    update

--- framework/src/apps/portal/views/portal.app.js
+++ framework/src/apps/portal/views/portal.app.js
@@ -1,37 +1,16 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import Icon from "@material-ui/icons/ImportContactsTwoTone";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { IconButton } from "@material-ui/core";
 import MenuBar from './portal.menubar';
 import NavBar from './portal.navbar';
 import Dashboard from "../../../pages/dashboard/views/dashboard.app";
+import '../design/portal.css';
 
 export default function() {
     const [isOpen, setIsOpen] = React.useState(true);
-    
     const theme = useTheme();
-    const useStyles = makeStyles((theme) => ({
-        root: {
-            height: "100%",
-            position: "absolute",
-            left: 0,
-            width: "100%",
-            overflow: "hidden"
-        },   
-        drawerDiv: {
-            height: "100%"
-        },
-        appBar: {
-          position: "relative"
-        },    
-        frame: {
-            display: "flex",
-            overflow: "hidden",
-            position: "relative",
-            width: "100%",
-            height: "100%"
-        },    
-    }));
+    const useStyles = makeStyles((theme) => ({ }));
     const classes = useStyles();
 
     function menuClick() {
@@ -43,10 +22,10 @@ export default function() {
     }
 
     return (
-        <div className={classes.root}>
-            <div className={classes.drawerDiv}>
+        <div className={'root'}>
+            <div className={'drawerDiv'}>
                 <MenuBar isOpen={isOpen} isOpenChanged={isOpenChanged}/>    
-                <div className={classes.frame}>
+                <div className={'frame'}>
                     <NavBar isOpen={isOpen}/>
                     <main>
                         <Dashboard/>
