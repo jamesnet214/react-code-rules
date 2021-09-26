705a5ed2d578b4a2ef8580c39a88ff3310359710
james lee <james.lee@devncore.org>
2021-09-26 23:38:26 +0900

    Update

--- framework/src/apps/portal/components/AppTitleTypo.js
+++ framework/src/apps/portal/components/AppTitleTypo.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { Typography } from '@material-ui/core';
 
 export default function AppTitleTypo(props) {
     const { title } = props;
+    const theme = useTheme();
+    const useStyles = makeStyles((theme) => ({ }));
+    const classes = useStyles();
     
     return (
         <Typography variant="h6"
--- framework/src/apps/portal/components/MailButton.js
+++ framework/src/apps/portal/components/MailButton.js
@@ -1,7 +1,12 @@
 import MailIcon from '@material-ui/icons/Mail';
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { IconButton, Badge } from '@material-ui/core';
 
 export default function MailButton() {
+    const theme = useTheme();
+    const useStyles = makeStyles((theme) => ({ }));
+    const classes = useStyles();
+    
     return (
         <IconButton size="large" 
                     aria-label="show 4 new mails"
