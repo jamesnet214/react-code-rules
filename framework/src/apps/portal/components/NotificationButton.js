e1563b8cfb94dc820d16312cf738c824779e4a80
james lee <james.lee@devncore.org>
2021-09-26 23:36:43 +0900

    Update

--- framework/src/apps/portal/components/NotificationButton.js
+++ framework/src/apps/portal/components/NotificationButton.js
@@ -1,7 +1,13 @@
+import React from "react";
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { IconButton, Badge } from '@material-ui/core';
 
 export default function NotificationButton() {
+    const theme = useTheme();
+    const useStyles = makeStyles((theme) => ({ }));
+    const classes = useStyles();
+
     return (
         <IconButton size="large"
                     aria-label="show 17 new notifications"
