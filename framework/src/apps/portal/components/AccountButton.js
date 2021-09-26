d073b7df04a0464fba68832b661826d61a4875c1
james lee <james.lee@devncore.org>
2021-09-26 23:38:51 +0900

    update

--- framework/src/apps/portal/components/AccountButton.js
+++ framework/src/apps/portal/components/AccountButton.js
@@ -1,7 +1,12 @@
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { IconButton } from '@material-ui/core';
 
 export default function AccountButton() {
+    const theme = useTheme();
+    const useStyles = makeStyles((theme) => ({ }));
+    const classes = useStyles();
+    
     return (
         <IconButton size="large"
                     edge="end"
