2641e7df7e4be1ea376d04e1304d6a0b2f3a4efc
james lee <james.lee@devncore.org>
2021-09-26 23:37:51 +0900

    Update

--- framework/src/apps/portal/components/MenuButton.js
+++ framework/src/apps/portal/components/MenuButton.js
@@ -1,9 +1,14 @@
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import { IconButton } from '@material-ui/core';
 
 export default function MenuButton(props) {
     const { isOpen, menuClick } = props;
+    const theme = useTheme();
+    const useStyles = makeStyles((theme) => ({ }));
+    const classes = useStyles();
+
     return (
         <IconButton size="large"
                     edge="start"
--- framework/src/apps/portal/components/MenuTreeView.js
+++ framework/src/apps/portal/components/MenuTreeView.js
@@ -2,9 +2,14 @@ import React from "react";
 import DateRangeIcon from '@material-ui/icons/DateRange';
 import InboxIcon from "@material-ui/icons/Inbox";
 import MemoryIcon from '@material-ui/icons/Memory';
+import { makeStyles, useTheme } from "@material-ui/core/styles";
 import MenuItem from './MenuTreeViewItem';
 
 export default function MenuTreeView() {
+  const theme = useTheme();
+  const useStyles = makeStyles((theme) => ({ }));
+  const classes = useStyles();
+
   return (
     <div>
         <MenuItem pages={[1, 2, 3]} header={'System'} icon={<MemoryIcon/>}/>
