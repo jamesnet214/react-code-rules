297c2a1416480389479423b1ba8c5724bd5d472e
james.lee <james.lee@devncore.org>
2021-09-27 16:25:34 +0900

    Create AgChart.jsx

---
+++ framework/src/components/chart/AgChart.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { AgChartsReact } from 'ag-charts-react';
+import axios from 'axios';
+
+class ChartExample extends Component {
+    constructor(props) {
+        super(props);
+        const ValueKeys = this.props.ykeys.split(',');
+        const ValueNames = this.props.ynames.split(',');
+
+    }
+}
